b7fadaa05be1052f4303c800750ad372016bbf9b
VIVAFROG <vivafrog@gmail.com>
2018-05-17 05:23:41 +0900

    1-3

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ const name = "Yoonsik",
     age = 30,
     gender = "male";
 
-const sayHi = (name, age, gender?) => {
+const sayHi = (name, age, gender) => {
     console.log(`Hello ${name}, you are ${age}, you are ${gender}`);
 };
 
-sayHi(name,age );
+sayHi(name,age,gender);
 
 export{};
