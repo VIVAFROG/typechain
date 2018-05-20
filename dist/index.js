c3bcd88a1474c2c2f3e801b9b20c7403bda205b8
VIVAFROG <vivafrog@gmail.com>
2018-05-20 12:05:08 +0900

    1-7

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const yoonsik = new Human("Yoonsik", 30, "male");
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, you are ${person.gender}`;
-};
-console.log(sayHi(yoonsik));
+const genesisBlkck = new Block(0, "202020202020", "", "Hello", 123456);
+let blockChain = [genesisBlkck];
+console.log(blockChain);
 //# sourceMappingURL=index.js.map
