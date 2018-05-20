6138f24eee66d4da374f6748bedf28df3e7670b4
VIVAFROG <vivafrog@gmail.com>
2018-05-20 11:12:37 +0900

    1-4

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const name = "Yoonsik", age = 30, gender = "male";
 const sayHi = (name, age, gender) => {
-    return console.log(`Hello ${name}, you are ${age}, you are ${gender}`);
+    return `Hello ${name}, you are ${age}, you are ${gender}`;
 };
-sayHi(name, age, gender);
+console.log(sayHi(name, age, gender));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ const name = "Yoonsik",
     age = 30,
     gender = "male";
 
-const sayHi = (name:string, age:number, gender:string) => {
-    return console.log(`Hello ${name}, you are ${age}, you are ${gender}`);
+const sayHi = (name:string, age:number, gender:string):string => {
+    return `Hello ${name}, you are ${age}, you are ${gender}`;
 };
 
-sayHi(name,age,gender);
+console.log(sayHi(name,age,gender));
 
 export{};
